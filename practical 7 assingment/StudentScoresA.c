6d9cb9223e7b1a9b0710e1ac1c755579f077b7e0
Reshav <kreshav24@tbc.edu.np>
2026-03-27 07:29:37 +0545

    Add files via upload

---
+++ practical 7 assingment/StudentScoresA.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+    int num=42;
+    int *ptr;  
+    ptr=&num;
+
+    printf("Value of num: %d\n", num);
+    printf("Address of num: %p\n", (void*)&num);
+    printf("Value of ptr: %p\n", (void*)ptr);
+    printf("Value pointed to by ptr: %d\n", *ptr);
+
+    *ptr=100;
+
+    printf("\nAfter modification through pointer:\n");
+    printf("Value of num: %d\n", num);
+    printf("Value pointed to by ptr: %d\n", *ptr);
+
+    return 0;
+}
